27c0f341e5cc7d8ee412caa9e2b2fc637ab7221d
bulbul-rasel <rasel.bulbul7@gmail.com>
2022-05-23 01:35:53 +0600

    make admin done

--- src/Dashboard/UserRow.js
+++ src/Dashboard/UserRow.js
@@ -1,12 +1,39 @@
 import React from 'react';
+import { toast } from 'react-toastify';
 
 const UserRow = ({ user, index, refetch }) => {
     const { email, role } = user;
+
+    const makeAdmin = () => {
+        fetch(`http://localhost:5000/user/admin/${email}`, {
+            method: 'PUT',
+            headers: {
+                authorization: `Bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+            })
+        // .then(res => {
+        //     if (res.status === 403) {
+        //         toast.error("Failed to make an Admin")
+        //     }
+        //     return res.json()
+        // })
+        // .then(data => {
+        //     if (data.modifiedCount > 0) {
+
+        //         refetch()
+        //         toast.success(`Successfully Made an Admin`);
+        //     }
+        // })
+    }
     return (
         <tr>
             <th>{index + 1}</th>
             <td>{email}</td>
-            <td>{role !== 'admin' && <button class="btn btn-xs btn-success">Make Admin</button>}</td>
+            <td>{role !== 'admin' && <button onClick={makeAdmin} class="btn btn-xs btn-success">Make Admin</button>}</td>
             <td><button class="btn btn-xs btn-warning">Remove User</button></td>
         </tr>
     );
