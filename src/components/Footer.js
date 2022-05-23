7b7afa388dfcbc3db81e8ea2683a54495305f8f6
bulbul-rasel <rasel.bulbul7@gmail.com>
2022-05-24 03:12:59 +0600

    add footer new

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,33 +4,45 @@ const Footer = () => {
     const today = new Date();
     const year = today.getFullYear();
     return (
-        <footer className=" p-10 bg-neutral mt-20">
-            <div className='footer'>
+        <div className='mt-10'>
+            <footer class="footer p-10 bg-base-200 text-base-content">
                 <div>
-                    <span className="footer-title">Services</span>
-                    <a className="link link-hover">Branding</a>
-                    <a className="link link-hover">Design</a>
-                    <a className="link link-hover">Marketing</a>
-                    <a className="link link-hover">Advertisement</a>
+                    <span class="footer-title">Services</span>
+                    <a class="link link-hover">Branding</a>
+                    <a class="link link-hover">Design</a>
+                    <a class="link link-hover">Marketing</a>
+                    <a class="link link-hover">Advertisement</a>
                 </div>
                 <div>
-                    <span className="footer-title">Company</span>
-                    <a className="link link-hover">About us</a>
-                    <a className="link link-hover">Contact</a>
-                    <a className="link link-hover">Jobs</a>
-                    <a className="link link-hover">Press kit</a>
+                    <span class="footer-title">Company</span>
+                    <a class="link link-hover">About us</a>
+                    <a class="link link-hover">Contact</a>
+                    <a class="link link-hover">Jobs</a>
+                    <a class="link link-hover">Press kit</a>
                 </div>
                 <div>
-                    <span className="footer-title">Legal</span>
-                    <a className="link link-hover">Terms of use</a>
-                    <a className="link link-hover">Privacy policy</a>
-                    <a className="link link-hover">Cookie policy</a>
+                    <span class="footer-title">Legal</span>
+                    <a class="link link-hover">Terms of use</a>
+                    <a class="link link-hover">Privacy policy</a>
+                    <a class="link link-hover">Cookie policy</a>
                 </div>
-            </div>
+                <div>
+                    <span class="footer-title">Newsletter</span>
+                    <div class="form-control w-80">
+                        <label class="label">
+                            <span class="label-text">Enter your email address</span>
+                        </label>
+                        <div class="relative">
+                            <input type="text" placeholder="username@site.com" class="input input-bordered w-full pr-16" />
+                            <button class="btn btn-primary absolute top-0 right-0 rounded-l-none">Subscribe</button>
+                        </div>
+                    </div>
+                </div>
+            </footer>
             <div className='my-10 text-center'>
                 <p>Copyright © {year} - All right reserved </p>
             </div>
-        </footer>
+        </div>
     );
 };
 
